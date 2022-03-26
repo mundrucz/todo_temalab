9e32376296ad41691299df43a24464063cae6c84
mundrucz <mundruczdzsesszika@gmailcom>
2022-03-26 15:07:57 +0100

    oszlopok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {DragDropContext, Droppable, Draggable} from "react-beautiful-dnd";
+import _ from "lodash";
+import {v4} from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+
+  const [state, setState]=useState({
+    "todo": {
+      title: "Todo",
+      items: ["f"]
+    },
+    "inprogress": {
+      title: "In Progress",
+      items: []
+    },
+    "done": {
+      title: "Completed",
+      items: []
+    },
+    "blocked":{
+      title: "Blocked",
+      items: ["ff"]
+    },
+  })
+
+  
+  const [title, setTitle] = React.useState('');
+  const [note, setNote] = React.useState('');
+  const [deadline, setDeadline] = React.useState('');
+  
+
+  function addNew(t, n, d){
+    setState(prev => {
+      return {
+        ...prev,
+        todo: {
+          title: "Todo",
+          items: [{
+            id:v4(),
+            title:t,
+            note:n,
+            deadline:d
+          },
+            ...prev.todo.items
+          ]
+        }
+      }
+    })
+
+     
+  setDeadline("")
+  setNote("")
+  setTitle("")
+  }
+
+
+  return(
+    <div>
+      title:
+      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
+      note:
+      <input type="text" value={note} onChange={(e) => setNote(e.target.value)}/>
+      deadline:
+      <input type="date" value={deadline} onChange={(e) => setDeadline(e.target.value)}/>
+  
+      <button type="button" onClick={() => addNew(title, note, deadline)}>
+      Add
+      </button>
+
+      <div style={{ display: "grid", gridTemplateColumns: "repeat(4, 1fr)", gridGap: 20 }}>
+        <div className='coloumn'>
+          {state.todo.title}
+          {state.todo.items.map(item =>
+          <div>
+            {item.title}, {item.note},{item.deadline}
+          </div>)
+          }
+        </div>
+
+        <div className='coloumn'>
+          {state.inprogress.title}
+          {state.inprogress.items.map(item =>
+          <div>
+            {item.title}, {item.note},{item.deadline}
+          </div>)
+          }
+        </div>
+
+        <div className='coloumn'>
+          {state.done.title}
+          {state.done.items.map(item =>
+          <div>
+            {item.title}, {item.note},{item.deadline}
+          </div>)
+          }
+        </div>
+
+        <div className='coloumn'>
+          {state.blocked.title}
+          {state.blocked.items.map(item =>
+          <div>
+            {item.title}, {item.note},{item.deadline}
+          </div>)
+          }
+        </div>
+        </div>
+      
     </div>
-  );
+      
+    );
+  
 }
 
 export default App;
+
+
